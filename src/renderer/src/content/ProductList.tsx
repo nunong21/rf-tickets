0f3eaecc7055c5294f0fe216593e34b1bdd938ff
Nuno Almeida <nuno.almeida@visma.com>
2024-06-01 13:45:11 +0100

    Vila Verde

--- src/renderer/src/content/ProductList.tsx
+++ src/renderer/src/content/ProductList.tsx
@@ -5,12 +5,12 @@ export const ProductList: ITProduct[] = [
     id: 4,
     name: 'Sumos',
     image: null,
-    price: 1.5,
+    price: 1.2,
     buttonColor: 'bg-red-500'
   },
   {
     id: 2,
-    name: 'Fino',
+    name: 'Cerveja',
     image: null,
     price: 1.2,
     buttonColor: 'bg-amber-300',
@@ -18,17 +18,24 @@ export const ProductList: ITProduct[] = [
   },
   {
     id: 3,
-    name: 'Fino Metro',
+    name: 'Cerveja S/ Alcool',
     image: null,
-    price: 11,
+    price: 1.2,
     buttonColor: 'bg-amber-300',
     buttonTextColor: null
   },
   {
     id: 7,
-    name: 'Copo vinho',
+    name: 'Vinho',
     image: null,
-    price: 0.7,
+    price: 0.6,
+    buttonColor: 'bg-purple-400'
+  },
+  {
+    id: 11,
+    name: 'Garrafa Vinho',
+    image: null,
+    price: 4,
     buttonColor: 'bg-purple-400'
   },
   {
@@ -49,14 +56,14 @@ export const ProductList: ITProduct[] = [
     id: 9,
     name: 'Martini',
     image: null,
-    price: 1.5,
+    price: 1.2,
     buttonColor: 'bg-amber-600'
   },
   {
     id: 10,
     name: 'Martini c/ cerveja',
     image: null,
-    price: 2.5,
+    price: 1.9,
     buttonColor: 'bg-amber-600'
   }
 ]
@@ -66,22 +73,15 @@ export const ProductListFood: ITProduct[] = [
     id: 64,
     name: 'Bifana',
     image: null,
-    price: 3.5,
+    price: 2.5,
     buttonColor: 'bg-yellow-600'
   },
   {
     id: 63,
     name: 'Caldo Verde',
     image: null,
-    price: 2,
+    price: 1.5,
     buttonColor: 'bg-green-600'
-  },
-  {
-    id: 60,
-    name: 'Frango + Pão',
-    image: null,
-    price: 10,
-    buttonColor: 'bg-green-500'
   },
   {
     id: 89,
@@ -89,21 +89,7 @@ export const ProductListFood: ITProduct[] = [
     image: null,
     price: 5,
     buttonColor: 'bg-amber-500'
-  },
-  {
-    id: 90,
-    name: 'Batata frita',
-    image: null,
-    price: 2,
-    buttonColor: 'bg-amber-500'
-  },
-  {
-    id: 62,
-    name: 'Pão',
-    image: null,
-    price: 1,
-    buttonColor: 'bg-blue-600'
-  },
+  }
 ]
 
 export const ProductListBundles: ITProduct[] = []
